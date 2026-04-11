0b30ae0f10f7557b71904c928feb44501dd6c49c
jvrbatista <jreisbatista10@gmail.com>
2026-04-11 16:03:13 -0300

    feat: login, cadastro e bloqueio de cartão

--- bank.js
+++ bank.js
@@ -3,11 +3,10 @@ import { stdin as input, stdout as output } from 'process';
 const rl = readline.createInterface({ input, output });
 
 //variaveis
-let saldoCliente = 2500;
-let extrato = [];
+let listaCadastro = [];
 let pontosSuspeita = 0;
-let cartaoBloqueado = false;
 let saquesSeguidos = 0;
+let usuarioCadastrado = null;
 
 //função para cliente depositar dinheiro na conta
 function depositar (saldoCLiente, valorDepositar) {
@@ -25,46 +24,79 @@ function sacar (saldoCLiente, valorSaque) {
     }
 };
 
-//interface para usuário
-    console.log(`Olá cliente do BANK, Seja bem-vindo!`);
+//Tela de cadastro para usuário
+async function telaAutenticacao () {
+    console.log("\nOlá cliente do BANK, Seja bem-vindo!");
 
-while (true)   {
-    //Se cartão estiver bloqueado, o sistema bancário não funciona
-    if (cartaoBloqueado === true) {
-        console.log("Cartão Bloqueado. Ligue para central de atendimento.")
-        break
+    while (true ) {
+    console.log("============ CADASTRO DO BANCO BANK ============")
+    let logout = await rl.question("1-Cadastrar\n2-Login\n\nQual serviço você deseja hoje?: ")
+        if (logout == "1") {
+            let nomeCliente = await rl.question("Cadastre o seu nome de usuário: ");
+            let senhaCliente = await rl.question("Cadastre a sua senha: ");
+            listaCadastro.push({
+                nome: nomeCliente,
+                senha: senhaCliente,
+                saldoCliente: 0,
+                extrato: [],
+                cartaoBloqueado: false                    
+            })}
+
+        if (logout == "2") {
+            let nomeCadastro = await rl.question("Digite seu nome de usuário: ");
+            let senhaCadastro = await rl.question("Digite sua senha para entrar: ")
+
+            usuarioCadastrado = listaCadastro.find(dadoscadastrais => dadoscadastrais.nome === nomeCadastro && dadoscadastrais.senha === senhaCadastro);
+                if (usuarioCadastrado) {
+                    if (usuarioCadastrado.cartaoBloqueado === true) {
+                        //Se cartão estiver bloqueado, o sistema bancário não funciona
+                        console.log("Cartão Bloqueado. Ligue para central de atendimento.")
+                    } else{
+                        //Se não estiver bloqueado, boas vindas ao usuário
+                    console.log(`Bem-vindo, ${usuarioCadastrado.nome}!`);
+                    break
+                    }
+                } else {
+                    console.log("Usuário ou senha incorretos.")
+                }
+        }
     }
-    console.log("========== MENU ==========");
-    let solicitacao = await rl.question("1-Sacar\n2-Depositar\n3-Verificar Extrato\n4-Sair\n\nQual serviço você deseja hoje?: ")
+}
+    await telaAutenticacao();
+    //Menu após login
+    while (true)   {
+    console.log("\n========== MENU ==========");
+    let solicitacao = await rl.question("1-Sacar\n2-Depositar\n3-Verificar Extrato\n4-Voltar ao menu\n\nQual serviço você deseja hoje?: ")
 
         if (solicitacao == "1") {
             let valorSaque = Number(await rl.question("Qual valor deseja sacar? R$"));
                 if (valorSaque > 1000) {
                 pontosSuspeita += 1;
                 }
-            saldoCliente = sacar(saldoCliente, valorSaque);
-            extrato.push(`Saque de R$${valorSaque} - Saldo: R$${saldoCliente}`);
-            console.log(`Saldo atual: R$${saldoCliente}`);
+            usuarioCadastrado.saldoCliente = sacar(usuarioCadastrado.saldoCliente, valorSaque);
+            usuarioCadastrado.extrato.push(`Saque de R$${valorSaque} - Saldo: R$${usuarioCadastrado.saldoCliente}`);
+            console.log(`Saldo atual: R$${usuarioCadastrado.saldoCliente}`);
             saquesSeguidos += 1;
         }
 
         if (solicitacao == "2") {
             let valorDepositar = Number(await rl.question("Qual valor deseja depositar? R$"));
-            saldoCliente = depositar(saldoCliente, valorDepositar);
-            extrato.push(`Depósito de R$${valorDepositar} - Saldo: R$${saldoCliente}`)
-            console.log(`Saldo atual: R$${saldoCliente}`)
+            usuarioCadastrado.saldoCliente = depositar(usuarioCadastrado.saldoCliente, valorDepositar);
+            usuarioCadastrado.extrato.push(`Depósito de R$${valorDepositar} - Saldo: R$${usuarioCadastrado.saldoCliente}`)
+            console.log(`Saldo atual: R$${usuarioCadastrado.saldoCliente}`)
         } 
 
         if (solicitacao == "3") {
             console.log("========== EXTRATO ==========")
-            for (let linha of extrato) {
+            for (let linha of usuarioCadastrado.extrato) {
                 console.log(linha);
             }
         }
 
         if (solicitacao == "4") {
-            console.log("Sistema finalizado.")
-         break
+            console.log(`Sistema Finalizado. Até mais ${usuarioCadastrado.nome}`);
+            usuarioCadastrado = null;
+            await telaAutenticacao();
         } 
 
         if (!["1","2","3","4"].includes(solicitacao)) {
@@ -72,13 +104,14 @@ while (true)   {
             break
         }
 
-        if (saquesSeguidos >= 3) {
+        if (saquesSeguidos > 3) {
             pontosSuspeita += 1
         }
 
         if (pontosSuspeita >= 2) {
-            cartaoBloqueado = true
+            usuarioCadastrado.cartaoBloqueado = true
             console.log("Muitos saques seguidos. Seu cartão foi bloqueado.")
+            await telaAutenticacao();
         }
     }
 
