c3104163fc72ce23cc3709979cf2496c34ea9f72
jvrbatista <jreisbatista10@gmail.com>
2026-07-19 12:26:29 -0300

    feat: Adicionar interceptor para deslogar automaticamente em sessao invalidada

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,22 @@
 import axios from 'axios'
+import { removerToken } from './storage.js'
 
 const api = axios.create({
     baseURL: 'https://bankjs-production.up.railway.app'
 })
+    api.interceptors.response.use(
+    (resposta) => resposta,
+    (error) => {
+        if (
+    (error.response?.status === 401 || error.response?.status === 403) &&
+    error.config.headers.Authorization
+    ) {
+        removerToken('usuario')
+        removerToken('gestor')
+        window.location.href = '/'
+    }
+        return Promise.reject(error)
+    }
+)
 
 export default api
